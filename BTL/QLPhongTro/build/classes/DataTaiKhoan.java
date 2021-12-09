29b31775440b0189e15fe4ff5eec688aa4f0354f
Dương <95034902+Duongdai460@users.noreply.github.com>
2021-12-09 22:38:38 +0700

    daiduong add file Helpers

---
+++ BTL/QLPhongTro/build/classes/DataTaiKhoan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Helpers;
+
+import Models.TaiKhoan;
+
+/**
+ *
+ * @author admin
+ */
+public class DataTaiKhoan {
+    public static TaiKhoan taiKhoan;
+}
